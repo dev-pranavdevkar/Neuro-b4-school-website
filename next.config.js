f9a514fc40999841d3348d55627817f62c427430
Pranav Devkar <109267896+devpranavdevkar@users.noreply.github.com>
2024-03-23 10:26:03 +0530

    deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { dev, isServer }) => {
+    // Add webpack configurations here
+    // Example: Add a custom alias for directories
+    config.resolve.alias['@components'] = path.resolve(__dirname, 'components');
+
+    // Return the modified webpack config
+    return config;
+  }
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
