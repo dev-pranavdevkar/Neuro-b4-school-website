943bfa312c33d505ddd988844c083995f7b6ff49
Pranav Devkar <109267896+devpranavdevkar@users.noreply.github.com>
2024-05-28 11:12:31 +0530

    Font isuue test on serve

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,24 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 // import ThemeButton from '@/component/Element/ThemeButton/ThemeButton'
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
 
+const cache = createCache({
+  key: 'css',
+  prepend: true,
+});
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
-        <Main />
-        <NextScript />
-        {/* <ThemeButton/> */}
-      </body>
+      <CacheProvider value={cache}>
+        <body>
+
+          <Main />
+          <NextScript />
+          {/* <ThemeButton/> */}
+        </body>
+      </CacheProvider>
     </Html>
   )
 }
